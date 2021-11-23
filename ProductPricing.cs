3eaf64f2dc5c7da170e2ab9e31165dbf4fdfcd07
yassine ghazzai <yassine.ghazzai@esprit.tn>
2021-11-23 01:10:29 +0100

    Test case no Product

--- ProductPricing.cs
+++ ProductPricing.cs
@@ -6,5 +6,16 @@ namespace Supermarket_Pricing
 {
     public class ProductPricing
     {
+        public decimal Pricing(string product)
+        {
+            if (String.IsNullOrEmpty(product))
+            {
+                return 0;
+            }
+
+            return -1;
+
+        }
+
     }
 }
--- ProductPricingTest.cs
+++ ProductPricingTest.cs
@@ -6,5 +6,18 @@ namespace Supermarket_Pricing
 {
     public class ProductPricingTest
     {
+        private ProductPricing productPricing;
+
+        public ProductPricingTest()
+        {
+            productPricing = new ProductPricing();
+        }
+
+        [Fact]
+        public void No_Product_returnsZero()
+        {
+            Assert.Equal(0, productPricing.Pricing(""));
+        }
+
     }
 }
